0502fbce5b0967591a1c68a07be351068d4cb4c3
Warren Jones <warren@pencethren.org>
2020-11-03 22:04:23 +0000

    Add enumerations to models.

--- app/models/presence.rb
+++ app/models/presence.rb
@@ -1,2 +1,4 @@
 class Presence < ApplicationRecord
+    enum statuses: { working: 'working', maybe: 'maybe', off: 'off' }
+    validates :status, inclusion: { in: statuses.keys }
 end
--- app/models/working_date.rb
+++ app/models/working_date.rb
@@ -1,2 +1,4 @@
 class WorkingDate < ApplicationRecord
+    enum date_types: { weekday: 'weekday', weekend: 'weekend', holiday: 'holiday', special: 'special' }
+    validates :day_type, inclusion: { in: date_types.keys } 
 end
